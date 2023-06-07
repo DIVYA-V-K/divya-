284f1304b77b15d47af16f22e7f09716cc77cee2
DIVYAV2005 <133838007+DIVYAV2005@users.noreply.github.com>
2023-06-07 07:04:51 +0000

    13

--- program/p13.py
+++ program/p13.py
@@ -1,18 +1,17 @@
-def calculate_tip():
-    food_rating = int(input("enter the food rating(1-5)"))
+food_rating = int(input("enter the food rating(1-5)"))
     service_rating = int(input("enter the service rating (1-5)"))
     ambience_rating = int(input("enter the ambience rating(1-5)"))
     bill_amount = float(input("enter the bill amount")) 
-if food_rating >= 4 and service_rating >= 4 and ambience_rating >=4:
+  if food_rating >= 4 and service_rating >= 4 and ambience_rating >=4:
     tip_ercentage 0.1
-elif food_rating >=3 and service_rating >=3 and ambience_rating >=3: 
+  elif food_rating >=3 and service_rating >=3 and ambience_rating >=3: 
     tip_percentage = 0.05
-elif food_rating >= 3:
+  elif food_rating >= 3:
     tip_percentage = 0.05
-else:
+  else:
     tip_percentage = 0.01
-tip_amount = bill_amount * tip_percentage
-total_amount = bill_amount + tip_amount  
-print("bill amount :",bill_amount)
-print("tip amount :",tip_amount)
-print("total amount :",total_amount)
+  tip_amount = bill_amount * tip_percentage
+  total_amount = bill_amount + tip_amount  
+  print("bill amount :",bill_amount)
+  print("tip amount :",tip_amount)
+  print("total amount :",total_amount)
