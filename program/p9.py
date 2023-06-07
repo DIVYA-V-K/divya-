d6ce92487da5c0366d92421eab240e7827a04a27
DIVYAV2005 <133838007+DIVYAV2005@users.noreply.github.com>
2023-06-07 04:51:19 +0000

    p9

---
+++ program/p9.py
@@ -0,0 +1,7 @@
+n1 = int(input("enter a 1st number"))
+n2 = int(input("enter a 2nd number"))
+n3 = int(input("enter a 3rd number"))
+if n1**n1 + n2**n2 == n3**n3:
+    print("1")
+else:
+    print("0")
