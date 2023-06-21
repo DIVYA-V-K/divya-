fe478b18a27a835b3c5e99063bf0190a10885ac4
DIVYAV2005 <133838007+DIVYAV2005@users.noreply.github.com>
2023-06-21 04:12:23 +0000

    p13

--- program/p13.py
+++ program/p13.py
@@ -1,17 +1,17 @@
 food_rating = int(input("enter the food rating(1-5)"))
-    service_rating = int(input("enter the service rating (1-5)"))
-    ambience_rating = int(input("enter the ambience rating(1-5)"))
-    bill_amount = float(input("enter the bill amount")) 
-  if food_rating >= 4 and service_rating >= 4 and ambience_rating >=4:
-    tip_ercentage 0.1
-  elif food_rating >=3 and service_rating >=3 and ambience_rating >=3: 
-    tip_percentage = 0.05
-  elif food_rating >= 3:
-    tip_percentage = 0.05
-  else:
-    tip_percentage = 0.01
-  tip_amount = bill_amount * tip_percentage
-  total_amount = bill_amount + tip_amount  
-  print("bill amount :",bill_amount)
-  print("tip amount :",tip_amount)
-  print("total amount :",total_amount)
+service_rating = int(input("enter the service rating (1-5)"))
+ambience_rating = int(input("enter the ambience rating(1-5)"))
+bill_amount = float(input("enter the bill amount")) 
+if food_rating >= 4 and service_rating >= 4 and ambience_rating >=4:
+  tip_percentage = 0.1
+elif food_rating >=3 and service_rating >=3 and ambience_rating >=3: 
+  tip_percentage = 0.05
+elif food_rating >= 3:
+  tip_percentage = 0.05
+else:
+  tip_percentage = 0.01
+tip_amount = bill_amount * tip_percentage
+total_amount = bill_amount + tip_amount  
+print("bill amount :",bill_amount)
+print("tip amount :",tip_amount)
+print("total amount :",total_amount)
